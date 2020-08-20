73676d5886e25c63d9730c04d09016ee7969a0e4
Lubos Beran <lubos.beran@petitpress.sk>
2020-08-20 13:49:24 +0200

    Added some comments

--- nginx/public/alive.php
+++ nginx/public/alive.php
@@ -6,12 +6,16 @@
 
  // check redis I/O
  if ($redisStatus == "OK") {
+     // check if we have session in GET parameter
     if(isset($_GET['sess']) && strlen($_GET['sess']) > 10) {
         $sess = $_GET['sess'];
 
+        // kreate key based on some unique identifier (in our case session id)
         $key = "alive_".$_GET['sess'];
+        // set key to DB with last active timestamp
         $redis->set($key, date('Y-m-d H:i:s'));
 
+        // just for debug
         echo date('Y-m-d H:i:s');
     }
  }else{
--- nginx/public/getAlive.php
+++ nginx/public/getAlive.php
@@ -6,12 +6,23 @@
 
  // check redis I/O
  if ($redisStatus == "OK") {
+     // get all keys with prefix alive_
     $key = "alive_*";
     $alive = $redis->keys($key);
     $active = [];
+
+    // loop over users and compare dates
     foreach($alive as $user) {
+        //user == alive_PHPSESSID
+        // get user key
         $lastActivity = $redis->get($user);
+
+        //check last activity
         $diff = strtotime(date('Y-m-d H:i:s')) - strtotime($lastActivity);
+
+        // if last activity was less than second ago, add to array as active
+        // if last activity was more than second ago, remove from redis 
+        // this interval should be changed - more than 1 second is recommended
         if ($diff > 1) {
             $redis->del($user);
         }else{
@@ -19,6 +30,8 @@
         }
     }
 
+
+    // return as JSON
     echo json_encode($active);
 
  }else{
